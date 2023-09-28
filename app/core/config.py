53f97828236ca2006be87034f394ebf7088a1270
shahnawaz-pabon <s.pabon93@gmail.com>
2023-09-28 22:11:36 +0600

    basic configuration done for creating hashed and verify password

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,11 @@ class Settings(BaseSettings):
     MONGO_INITDB_ROOT_PASSWORD: str
     MONGO_INITDB_DATABASE: str
     MONGODB_URI: str
+    JWT_PUBLIC_KEY: str
+    JWT_PRIVATE_KEY: str
+    REFRESH_TOKEN_EXPIRES_IN: int
+    ACCESS_TOKEN_EXPIRES_IN: int
+    JWT_ALGORITHM: str
     # Add other configuration settings as needed
 
     class Config:
